4ae5059cfc1d0b5bfeb938461e745acde5bbabc6
Blues64 <Ethan.r.sharp@gmail.com>
2023-03-01 10:55:12 -0600

    addHowToAndStartPage

---
+++ js/home.js
@@ -0,0 +1,32 @@
+document.getElementById("howto").onclick = function () {
+    location.href = "howToPlay.html";
+  };
+  document.getElementById("play").onclick = function () {
+    location.href = "play.html";
+  };
+  var audio = new Audio('https://archive.org/download/twitter-1355278061181280262/1355278061181280262.mp4');
+document.getElementById("music").onclick = function(){
+
+    document.getElementById("music").style.display = 'none';
+   document.getElementById("musicOff").style.display = 'initial';
+
+audio.play();
+   
+   audio.addEventListener('ended', function() {
+   this.currentTime = 0;
+   this.play();
+
+}, false);
+}
+
+document.getElementById("musicOff").onclick = function(){
+  document.getElementById("music").style.display = 'initial';
+  document.getElementById("musicOff").style.display = 'none';
+
+  audio.pause();
+}
+
+document.getElementById("musicOff").style.display = 'none';
+
+
+
